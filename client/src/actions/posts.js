f2a00dcbc063d394774868b009cc63cb34dbc1d2
Mateusz Byrtus <mateusz.byrtus@gmail.com>
2022-09-18 19:49:43 +0200

    Added edit post functionality to client and server

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -9,11 +9,20 @@ export const getPosts = () => async (dispatch) => {
   }
 };
 
-export const createPost = (post) => async(dispatch)=>{
+export const createPost = (post) => async (dispatch) => {
   try {
-      const {data} = await api.createPost(post)
-      dispatch({type: "CREATE", payload: data})
+    const { data } = await api.createPost(post);
+    dispatch({ type: "CREATE", payload: data });
   } catch (error) {
     console.log(error.message);
   }
-}
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+    dispatch({ type: "UPDATE", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
