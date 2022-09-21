34e5244003c1e38d52f00b7c2228dc631ae10f74
Mateusz Byrtus <mateusz.byrtus@gmail.com>
2022-09-21 20:29:06 +0200

    added .evn file with sensitive credentials

--- server/index.js
+++ server/index.js
@@ -3,20 +3,22 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://mateuszbyrtus:mateuszbyrtus123@cluster0.g4qtsrf.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://mateuszbyrtus:mateuszbyrtus123@cluster0.g4qtsrf.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     return app.listen(PORT, () =>
       console.log(`Server running on port ${PORT}`)
